ebdbb339f31e6ec9165509986809892e2ebe9858
YiChenshu <ycswaves@gmail.com>
2014-04-11 00:17:54 +0800

    prefix-path-trimming

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ module.exports = function(){
 
   var myexpress = function(req, res) {
     next();
+    myexpress.stackPointer = 0;
+
 
     function next(err){
       layer = myexpress.stack[myexpress.stackPointer++];
-
-
       if(!layer){
         if(!err){
           res.statusCode = 404;
@@ -26,23 +26,30 @@ module.exports = function(){
         if(layer.handle.length < 4){
           next(err); //if it's not a error handle middleware, pass down the stack
         }
-        else{
+        else if(layer.match(req.url)){
           layer.handle(err, req, res, next);
+        }
+        else{
+          next(err);
         }
       }
       else{
         try {
           if(layer.match(req.url)){
+            //console.log('match');
             req.params = layer.match(req.url).params;
             if(layer.handle.length === 4){
               next();
             }
             else{
+              if(layer.embed){
+                req.url = req.url.replace(/^\/[^\/]*/,''); //trim prefix
+              }
               layer.handle(req, res, next);
-              myexpress.stackPointer = 0;
             }
           }
           else{
+            //console.log('no match');
             next();
           }
         } catch(e) {
@@ -60,23 +67,33 @@ module.exports = function(){
   }
 
   myexpress.use = function(path, fn){
-    if(typeof path.stack !== 'undefined'){
+    if(typeof path.stack !== 'undefined'){ // if 'path' is an app
       for (var i in path.stack ){
         myexpress.stack.push(path.stack[i]);
       }
     }
     else{
       var layerPath;
-      if(typeof path === 'function'){
-        layerPath = '/';
-        fn = path;
+      if(typeof path === 'function'){// a handler without path
+        var layer = new Layer('/', path);
+        myexpress.stack.push(layer);
       }
-      else{
-        layerPath = path;
+      else{// a handler with path, fn can be a handler or an app
+        if(typeof fn.stack !== 'undefined'){ // if fn is an app
+          for (var j in fn.stack ){
+            fn.stack[j].path = path+fn.stack[j].path;
+            fn.stack[j].embed = true;
+            myexpress.stack.push(fn.stack[j]);
+          }
+        }
+        else{ // fn is a handler
+          layerPath = path;
+          var layer = new Layer(layerPath, fn);
+          myexpress.stack.push(layer);
+        }
       }
-      var layer = new Layer(layerPath, fn);
-      myexpress.stack.push(layer);
     }
   }
+  myexpress.handle = myexpress;
   return myexpress;
 }
